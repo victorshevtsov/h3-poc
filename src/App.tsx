23f95c9021a35a69ea799e715e59ad8eb6846e83
Victor Shevtsov <victorshevtsov@gmail.com>
2021-09-23 21:46:10 +0300

    Add h3-js

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,48 @@
+import { Marker, Polygon } from '@react-google-maps/api';
+import * as h3 from 'h3-js';
 import React from 'react';
 import './App.css';
 import Map from './components/Map';
 
+const center = {
+  lat: 43.65,
+  lng: -79.38
+};
+
+const h3Index = h3.geoToH3(center.lat, center.lng, 8);
+
+function h3ToPolyline(h3Index: string) {
+  let hexBoundary = h3.h3ToGeoBoundary(h3Index)
+  hexBoundary.push(hexBoundary[0])
+
+  let arr = []
+  for (const i of hexBoundary) {
+    arr.push({ lat: i[0], lng: i[1] })
+  }
+
+  return arr
+}
+
+const polyline = h3ToPolyline(h3Index);
+
+const polygonOptions: google.maps.PolygonOptions = {
+  fillColor: "red",
+  strokeColor: "navy",
+  strokeWeight: 1
+}
+
 function App() {
   return (
-    <div className="App">
-      <Map />
+    <div className="App" >
+      {/* <Map center={center} /> */}
+      <Map center={center}>
+        <Marker position={center} opacity={0.8} />
+        <Polygon path={polyline} options={polygonOptions} />
+      </Map>
+      <Map center={center}>
+        <Marker position={center} opacity={0.8} />
+        <Polygon path={polyline} options={polygonOptions} />
+      </Map>
     </div>
   );
 }
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,29 +1,54 @@
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import { GoogleMap, useLoadScript } from '@react-google-maps/api';
 import React from 'react';
 
-const containerStyle = {
-  width: '100vW',
-  height: '100vh'
-};
-const center = {
-  lat: 43.65,
-  lng: -79.38
+const containerStyle: React.CSSProperties = {
+  width: '100%',
+  height: '100%'
 };
 
-function Map() {
-  return (
-    <LoadScript
-      googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY as string}
+function Map({ children, center }: any) {
+
+  const mapRef = React.useRef<GoogleMap>(null);
+
+  const mapOptions = (): google.maps.MapOptions => {
+    return {
+      center,
+      zoom: 14,
+      zoomControlOptions: { position: google.maps.ControlPosition.LEFT_CENTER, },
+      streetViewControlOptions: { position: google.maps.ControlPosition.LEFT_BOTTOM },
+      styles: [
+        { featureType: "poi", stylers: [{ "visibility": "off" }] },
+        { featureType: "transit", stylers: [{ "visibility": "off" }] },
+      ]
+    }
+  }
+
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY as string
+  })
+
+  const onBoundsChanged = () => {
+    console.log("onBoundsChanged", mapRef.current);
+  }
+  const onCenterChanged = () => {
+    console.log("onCenterChanged");
+  }
+
+  const renderMap = () => <>
+    <GoogleMap ref={mapRef}
+      mapContainerStyle={containerStyle}
+      options={mapOptions()}
+      onBoundsChanged={onBoundsChanged}
+      onCenterChanged={onCenterChanged}
     >
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={10}
-      >
-        <Marker position={center} opacity={0.8} />
-      </GoogleMap>
-    </LoadScript>
-  )
-}
+      {children}
+    </GoogleMap>
+  </>
 
-export default React.memo(Map)
+  if (loadError) {
+    return <div>Map cannot be loaded</div>
+  }
+
+  return isLoaded ? renderMap() : <div>Loading the map...</div>
+}
+export default Map;
